1bdbdae95f5bd86d9b66dadf0c125a4a6a1ea0ca
Andrey Rys <rys@lynxlynx.ru>
2022-08-02 20:52:13 +0200

    fix buildscripts

--- make_android.sh
+++ make_android.sh
@@ -2,4 +2,4 @@ if [ -z "${1}" ]; then
 	make clean
 	./configure --prefix=/system --sysconfdir=/system/etc --localstatedir=/data/misc --disable-pam --disable-lastlog --disable-utmp --disable-utmpx --disable-wtmp --disable-wtmpx --disable-shadow --enable-bundled-libtom --disable-syslog LDFLAGS=-lcrypt || exit 1
 fi
-make PROGRAMS="dropbear dropbearkey dropbearconvert scp dbclient" MULTI=1 || exit 1
+make PROGRAMS="dropbear dropbearkey dropbearconvert scp" MULTI=1 "${@}" || exit 1
